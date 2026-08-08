6fdd97a8b58bce2777376cb016ca683b066bad20
karmark <karmark@karmark.16030454.xyz>
2026-08-08 14:44:14 +0530

    building basic class hierarchy

--- models.py
+++ models.py
@@ -1,8 +1,16 @@
 
 
 
-
-
 class ApiBase():
     def __init__(self):
         pass
+    
+    def make_request(self):
+        pass
+    
+class Authentication(ApiBase):
+    pass
+
+class Tracking(Authentication):
+    pass
+

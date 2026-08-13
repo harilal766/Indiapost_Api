b430147417d072b543643ede484fad29a7ce80cb
karmark <karmark@karmark.16030454.xyz>
2026-08-13 16:14:20 +0530

    working on connection testing

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,8 +7,13 @@ class TestApiBase:
     instance = ApiBase(
         username=USERNAME, password=PASSWORD
     )
+    
     def test_username(self):
         username_check = re.fullmatch(r'\d{10}',self.instance.username)
         assert username_check
         assert self.instance.password == PASSWORD
-    
+
+
+    def test_make_request(self):
+        resp = self.instance.make_request(endpoint = 'login')
+        assert resp
